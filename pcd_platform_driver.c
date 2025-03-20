6f5d9a51b2b72874f646f8b9a715c8fafef1f737
Dinesh Bobburu <bobburudinesh123@gmail.com>
2025-03-20 11:32:31 -0500

    Implemented lseek, open read, write methods of Character driver platform driver.

--- pcd_platform_driver.c
+++ pcd_platform_driver.c
@@ -17,7 +17,7 @@
 loff_t pcd_lseek(struct file *filp, loff_t offset, int whence);
 ssize_t pcd_read(struct file *filp, char __user *buff, size_t count, loff_t *f_pos);
 ssize_t pcd_write(struct file *filp, const char __user *buff, size_t count, loff_t *f_pos);
-int pcd_open(struct inode *inode, struct file *flip);
+int pcd_open(struct inode *inode, struct file *filp);
 int pcd_release(struct inode *inode, struct file *filp);
 int check_permission(int dev_perm, int acc_mode); 
 int pcd_platform_driver_probe(struct platform_device *pdev);
@@ -95,19 +95,98 @@ int check_permission(int dev_perm, int acc_mode) {
 }
 
 loff_t pcd_lseek(struct file *filp, loff_t offset, int whence){
+	/*Get private data of char device inorder to get max size of character device*/
+	struct pcdev_private_data *pcdev_data = (struct pcdev_private_data*)filp->private_data;
+        int max_size = pcdev_data->pdata.size;
+	loff_t temp;
+	pr_info("lseek requested\n");
+	pr_info("Current value of the file position = %lld\n",filp->f_pos);
+
+	switch(whence){
+		case SEEK_SET:
+			if(offset>max_size || offset<0){
+				return -EINVAL;
+			}
+			filp->f_pos = offset;
+			break;
+		case SEEK_CUR:
+			temp = filp->f_pos + offset;
+			if((temp > max_size) || (temp < 0)){
+				return -EINVAL;
+			}
+			filp->f_pos = offset;
+			break;
+
+		case SEEK_END:
+			temp = filp->f_pos + offset;
+			if((temp > max_size) || (temp < 0)) {
+				return -EINVAL;
+			}
+			filp->f_pos = offset;
+			break;
+		default:
+			return -EINVAL;	
+	};
+	pr_info("New value of the file position = %lld\n",filp->f_pos);
 	return 0;
 }
 ssize_t pcd_read(struct file *filp, char __user *buff, size_t count, loff_t *f_pos){
+	
+	/*Get private data of char device inorder to get max size of character device*/
+	struct pcdev_private_data *pcdev_data = (struct pcdev_private_data*)filp->private_data;
+	int max_size = pcdev_data->pdata.size;
+	pr_info("read requested for %zu bytes\n", count);
+	pr_info("Current file position = %lld\n", *f_pos);
+	/*Check if count has to be adjusted*/
+	if((*f_pos + count) > max_size) {
+		count = max_size - *f_pos;
+	}
+	/*Read from kernel space buffer to user space buffer*/
+	if(!copy_to_user(buff,pcdev_data->buffer+(*f_pos),count)){
+		return -EFAULT;
+	}
+	/*Update the current file position*/
+	*f_pos += count;
+	pr_info("Number of bytes successfully read = %zu\n", count);
+	pr_info("Updated file position = %lld\n", *f_pos);
 	/*return number of bytes sucessfully read*/
-	return 0;
+	return count;
 }
 ssize_t pcd_write(struct file *filp, const char __user *buff, size_t count, loff_t *f_pos){
+	/*Get private data of char device inorder to get max size of character device*/
+	struct pcdev_private_data *pcdev_data = (struct pcdev_private_data*)filp->private_data;
+	int max_size = pcdev_data->pdata.size;
+	pr_info("write requested for %zu bytes\n", count);
+	pr_info("Current file position = %lld\n", *f_pos);
+	/*Check if count has to be adjusted*/
+	if((*f_pos + count)>max_size) {
+		count = max_size - *f_pos;
+	}
+	/*Write from user space buffer to kernel space buffer*/
+	if(!copy_from_user(pcdev_data->buffer+(*f_pos), buff, count)){
+		return -EFAULT;
+	}
+	/*Update the current file position*/
+	*f_pos += count;
+	pr_info("Nuber of bytes successfully written = %zu\n", count);
+	pr_info("Updated file position = %lld\n", *f_pos);
 	/*return number of bytes successfully written*/
-	return 0;
+	return count;
 }
-int pcd_open(struct inode *inode, struct file *flip){
-	pr_info("open was successful\n");
-	return 0;
+int pcd_open(struct inode *inode, struct file *filp){
+	int ret;
+	struct pcdev_private_data *pcdev_data;
+	int minor_num;
+	minor_num = MINOR(inode->i_rdev);
+	pr_info("Minor Number = %d\n", minor_num);
+	/*Get devices private data structure*/
+	pcdev_data = container_of(inode->i_cdev, struct pcdev_private_data, cdev);
+	/*to pass device private data to other methods of the driver, as filp is passed as argument to other functions*/
+	filp->private_data = pcdev_data;
+	/*Check Permissions*/
+	ret = check_permission(pcdev_data->pdata.perm, filp->f_mode);
+	(!ret) ? pr_info("open was successful\n") : pr_info("open was unsuccessful\n");
+	return ret;
 }
 int pcd_release(struct inode *inode, struct file *filp){
 	pr_info("release was successful\n");
